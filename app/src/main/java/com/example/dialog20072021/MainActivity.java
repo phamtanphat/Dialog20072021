79a94c3630254b5bf7d9a184fb18aecbf4e00ed3
phamtanphat <phatdroid94@gmail.com>
2021-09-07 18:32:41 +0700

    multiple choice va stringbuilder

--- app/src/main/java/com/example/dialog20072021/MainActivity.java
+++ app/src/main/java/com/example/dialog20072021/MainActivity.java
@@ -10,10 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     Button mBtnAlertDialog;
-    int mIndex = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +26,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Đường");
+                builder.setTitle("Thực đơn");
                 builder.setIcon(R.mipmap.ic_launcher);
                 builder.setCancelable(false);
 
-                String[] arraySugar = {"50%","60%","70%","80%","90%","100%"};
 
-                builder.setSingleChoiceItems(arraySugar, -1, new DialogInterface.OnClickListener() {
+                String[] arrayFood = {"Gà","Mỳ Ý","Nước ngọt","Salad"};
+
+                boolean[] arrayChecked = new boolean[arrayFood.length];
+
+                builder.setMultiChoiceItems(arrayFood, arrayChecked, new DialogInterface.OnMultiChoiceClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        mIndex = i;
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+
                     }
                 });
 
@@ -42,18 +46,25 @@ public class MainActivity extends AppCompatActivity {
                 builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this, arraySugar[mIndex], Toast.LENGTH_SHORT).show();
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (int y = 0; y < arrayChecked.length; y++) {
+                            if(arrayChecked[y]){
+                                stringBuilder.append(arrayFood[y] + ", ");
+                            }
+                        }
+                        stringBuilder.delete(stringBuilder.length() - 2 , stringBuilder.length() - 1);
+                        Toast.makeText(MainActivity.this, "Order của bạn là " +stringBuilder.toString(), Toast.LENGTH_SHORT).show();
                     }
                 });
 
                 // Negative Button (tiêu cực như là hủy hoặc không đồng ý)
-                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        mIndex = -1;
-                        Toast.makeText(MainActivity.this, "Bạn chưa chọn", Toast.LENGTH_SHORT).show();
-                    }
-                });
+//                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        mIndex = -1;
+//                        Toast.makeText(MainActivity.this, "Bạn chưa chọn", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
 //
 //                // Neutral button (chưa xác định)
 //                builder.setNeutralButton("Chưa xác định", new DialogInterface.OnClickListener() {
@@ -64,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
 //                });
 
                 builder.show();
+
+                // Thực đơn
+                // Gà
+                // mỳ ý
+                // nước ngọt
+                // salad
+                // ...
 
             }
         });
